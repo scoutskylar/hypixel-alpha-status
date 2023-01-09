1a8532a0776bcebbddef5e0f0c988685e5cb51fc
scoutskylar <scoutskylar@outlook.com>
2023-01-08 19:54:45 -0500

    Match HSW Bot
    * Improvements to webhook
    * Updated status calculation to match HSW Bot

--- discord-webhook.js
+++ discord-webhook.js
@@ -1,13 +1,40 @@
 const webhookURL = ''; // redacted for the public repository, of course
+const webhookUsername = "scoutskylar's Alpha Webhook";
 
 // adapted from https://dev.to/oskarcodes/send-automated-discord-messages-through-webhooks-using-javascript-1p01
-function sendWebhookMessage(message) {
+function sendWebhookMessage(title, message, color = 5814783) {
     if (!webhookURL) return;
     
     const request = new XMLHttpRequest();
     request.open("POST", webhookURL);
     request.setRequestHeader('Content-type', 'application/json');
-    request.send(JSON.stringify({ content: message }));
+    request.send(JSON.stringify({
+        "content": null,
+        "embeds": [
+            {
+                "title": title,
+                "description": message,
+                "color": color,
+                "author": {
+                    "name": "Alpha Hypixel Network Status 🔗",
+                    "url": "https://scoutskylar.github.io/hypixel-alpha-status/",
+                    "icon_url": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                },
+                "timestamp": new Date().toISOString()
+            }
+        ],
+        "username": webhookUsername,
+        "attachments": []
+    }));
+}
+
+function sendPlainWebhookMessage(message) {
+    if (!webhookURL) return;
+    
+    const request = new XMLHttpRequest();
+    request.open("POST", webhookURL);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send(JSON.stringify({ "content": message, "username": webhookUsername }));
 }
 
 if (webhookURL) {
--- script.js
+++ script.js
@@ -1,37 +1,57 @@
 const pingEndpointURL = 'https://untitled-7lce44jf0ekc.runkit.sh/json';
 
-var intervalID = 0;
-var currentStatusCode = -1;
+const statusNames = ['closed', 'maybe open', 'maybe open', 'open'];
 
-async function fetchAlphaStatus() {
-    return await fetch(`${pingEndpointURL}`).then(response => response.json());
+var intervalID = 0;
+var prevData = {};
+
+async function fetchAlphaData() {
+    return await fetch(pingEndpointURL).then(response => response.json());
 }
 
-function generateMessage(max, online) {
-    var statusCode = max === 0 ? 0 : (max > 50) * 2 + (online >= 20);
+function setOpen() { prevData.statusCode = 3; restart(); }
+function setClosed() { prevData.statusCode = 0; restart(); }
 
-    if (currentStatusCode >= 0 && currentStatusCode != statusCode) {
-        console.log(`Alpha status code changed from ${currentStatusCode} to ${statusCode}`);
-        sendWebhookMessage(`The Alpha Hypixel Network ${webhookMessages[statusCode]}.`);
+function generateMessage(max, online) {
+    var oldStatusCode = prevData.statusCode;
+    
+    var statusCode = oldStatusCode || 0;
+    if (max === 0) {
+        statusCode = 0;
+    } else if (online >= 50) {
+        statusCode = 3;
+    } else if (max > 50) {
+        if (online >= 20) {
+            statusCode = 3;
+        } else if (oldStatusCode !== 3) {
+            statusCode = 2;
+        }
+    } else if (online < 20 || oldStatusCode !== 3) {
+        statusCode = 0;
     }
-    currentStatusCode = statusCode;
+    
+    if (prevData.max != max && prevData.max !== undefined) {
+        // Player limit change
+        console.log(`Player limit changed from ${prevData.max} to ${max}`);
+        sendPlainWebhookMessage(`Player Limit Change: \`${prevData.online}/${prevData.max}\` :arrow_forward: \`${online}/${max}\``);
+    }
+    if (oldStatusCode != statusCode && oldStatusCode !== undefined) {
+        // Alpha status change
+        console.log(`Alpha status changed from ${statusNames[oldStatusCode]} to ${statusNames[statusCode]}`);
+        sendPlainWebhookMessage(`Alpha Status Change: \`${statusNames[oldStatusCode]}\` :arrow_forward: \`${statusNames[statusCode]}\``);
+    }
+    prevData = { online, max, statusCode };
     
     return [
-        `The <i>Alpha Hypixel Network</i> is currently <span style="color: #FF5555;">closed</span>.<br>The player limit is ${max}, which ${max == 0 ? '' : 'usually'} means the server is closed to the public.${online == 0 ? '' : `<br>There ${online == 1 ? 'is 1 player' : `are ${online} players`} online, but they are likely just ${online == 1 ? 'an admin' : `admins${online > 10 ? 'and testers' : ''}`}`}.`,
-        `The <i>Alpha Hypixel Network</i> is currently <span style="color: #FFFF55;">possibly open</span>.<br>The player limit is ${max}, which usually means the server is closed to the public.<br>However, there are ${online} players online, so it might actually be open.`,
-        `The <i>Alpha Hypixel Network</i> is currently <span style="color: #00AA00;">probably open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>However, there ${online == 0 ? "aren't any players" : (online == 1 ? 'is only 1 player' : `are only ${online} players`)} online, so the server may not actually be open.`,
+        `The <i>Alpha Hypixel Network</i> is currently <span style="color: #FF5555;">closed</span>.<br>The player limit is ${max}, which ${max == 0 ? '' : 'usually'} means the server is closed to the public.${online == 0 ? '' : `<br>There ${online == 1 ? 'is 1 player' : `are ${online} players`} online, but they are likely just ${online == 1 ? 'an admin' : `admins${online > 10 ? ' and testers' : ''}`}.`}`,
+        `The <i>Alpha Hypixel Network</i> is <span style="color: #FFFF55;">probably open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>There ${online == 0 ? "aren't any players" : (online == 1 ? 'is only 1 player' : `are only ${online} players`)} online, so the server may not actually be open.`,
+        `The <i>Alpha Hypixel Network</i> is <span style="color: #FFFF55;">probably open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>There ${online == 0 ? "aren't any players" : (online == 1 ? 'is only 1 player' : `are only ${online} players`)} online, so the server may not actually be open.`,
         `The <i>Alpha Hypixel Network</i> is currently <span style="color: #55FF55;">open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>There are ${online} players online, so there ${online - max > 0 ? `${online - max == 1 ? 'is 1 player' : `are ${online - max} players`} in the queue.` : 'is currently <span style="color: #55FF55;">no queue</span>.'}`
     ][statusCode];
 }
 
-const webhookMessages = [
-    'has **closed**',
-    'is **possibly open**',
-    'is **probably open**',
-    'is now **open**'
-]
 async function updateMessage() {
-    var alphaStatus = await fetchAlphaStatus();
+    var alphaStatus = await fetchAlphaData();
     console.log(alphaStatus);
     document.getElementById('status-with-slash').innerText = alphaStatus.online + '/' + alphaStatus.max;
     document.getElementById('alpha-status-message').innerHTML = generateMessage(alphaStatus.max, alphaStatus.online);
@@ -41,7 +61,7 @@ function stop() {
     clearInterval(intervalID);
 }
 
-function start() {
+function restart() {
     stop();
     updateMessage();
     intervalID = setInterval(updateMessage, 60000);
@@ -55,4 +75,4 @@ if (window.location.search.match(/[?&]popup/)) {
     document.getElementById('popup-button-container').style.display = 'none';
 }
 
-start();
+restart();
