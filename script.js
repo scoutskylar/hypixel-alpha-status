ca72418ea1d7d31dce47e0ebc65557c0073438e9
scoutskylar <scoutskylar@outlook.com>
2023-04-03 22:05:41 -0400

    Add internet status and more

--- script.js
+++ script.js
@@ -1,70 +1,63 @@
-const pingEndpointURL = 'https://untitled-7lce44jf0ekc.runkit.sh/json';
+var intervalDelay = 60000;
 
-const statusNames = ['closed', 'maybe open', 'maybe open', 'open'];
+const pingEndpointURL = 'https://7lce44jf0ekc.runkit.sh/json';
 
 var intervalID = 0;
 var prevData = {};
+var connected = true; // stores whether the browser client had a network connection at the last check
 
 async function fetchAlphaData() {
     return await fetch(pingEndpointURL).then(response => response.json());
 }
 
-function setOpen() { prevData.statusCode = 3; restart(); }
-function setClosed() { prevData.statusCode = 0; restart(); }
+function generateMessage(max, online, timestamp) {
+    var alphaOpen = max > 0 && online > 5 && (max > 60 || online > 30 || prevData.max <= 60 && prevData.alphaOpen);
 
-function generateMessage(max, online) {
-    var oldStatusCode = prevData.statusCode;
-    
-    var statusCode = oldStatusCode || 0;
-    if (max === 0) {
-        statusCode = 0;
-    } else if (online >= 50) {
-        statusCode = 3;
-    } else if (max > 60) {
-        if (online >= 20) {
-            statusCode = 3;
-        } else if (oldStatusCode !== 3) {
-            statusCode = 2;
-        }
-    } else if (online < 20 || oldStatusCode !== 3) {
-        statusCode = 0;
-    }
-    
     if (prevData.max != max && prevData.max !== undefined) {
         // Player limit change
-        console.log(`Player limit changed from ${prevData.max} to ${max}`);
+        console.log(`[${new Date(timestamp).toLocaleString()}] Player limit changed from ${prevData.max} to ${max}`);
         sendPlainWebhookMessage(`Player Limit Change: \`${prevData.online}/${prevData.max}\` :arrow_forward: \`${online}/${max}\``);
     }
-    if (oldStatusCode != statusCode && oldStatusCode !== undefined) {
+    if (prevData.alphaOpen ^ alphaOpen && prevData.alphaOpen !== undefined) {
         // Alpha status change
-        console.log(`Alpha status changed from ${statusNames[oldStatusCode]} to ${statusNames[statusCode]}`);
-        sendPlainWebhookMessage(`Alpha Status Change: \`${statusNames[oldStatusCode]}\` :arrow_forward: \`${statusNames[statusCode]}\``);
+        console.log(`[${new Date(timestamp).toLocaleString()}] Alpha status changed from ${prevData.alphaOpen ? 'open' : 'closed'} to ${alphaOpen ? 'open' : 'closed'}`);
+        sendPlainWebhookMessage(`Alpha Status Change: \`${prevData.alphaOpen ? 'open' : 'closed'}\` :arrow_forward: \`${alphaOpen ? 'open' : 'closed'}\``);
     }
-    prevData = { online, max, statusCode };
     
-    return [
-        `The <i>Alpha Hypixel Network</i> is currently <span style="color: #FF5555;">closed</span>.<br>The player limit is ${max}, which ${max == 0 ? '' : 'usually'} means the server is closed to the public.${online == 0 ? '' : `<br>There ${online == 1 ? 'is 1 player' : `are ${online} players`} online, but they are likely just ${online == 1 ? 'an admin' : `admins${online > 10 ? ' and testers' : ''}`}.`}`,
-        `The <i>Alpha Hypixel Network</i> is <span style="color: #FFFF55;">probably open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>There ${online == 0 ? "aren't any players" : (online == 1 ? 'is only 1 player' : `are only ${online} players`)} online, so the server may not actually be open.`,
-        `The <i>Alpha Hypixel Network</i> is <span style="color: #FFFF55;">probably open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>There ${online == 0 ? "aren't any players" : (online == 1 ? 'is only 1 player' : `are only ${online} players`)} online, so the server may not actually be open.`,
-        `The <i>Alpha Hypixel Network</i> is currently <span style="color: #55FF55;">open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>There are ${online} players online, so there ${online - max > 0 ? `${online - max == 1 ? 'is 1 player' : `are ${online - max} players`} in the queue.` : 'is currently <span style="color: #55FF55;">no queue</span>.'}`
-    ][statusCode];
+    prevData = { online, max, alphaOpen };
+    
+    return alphaOpen
+        ? `The <i>Alpha Hypixel Network</i> is currently <span style="color: #55FF55;">open</span> with a limit of <span style="color: #55FF55;">${max}</span> players.<br>There are ${online} players online, so there ${online - max > 0 ? `${online - max == 1 ? 'is 1 player' : `are ${online - max} players`} in the queue.` : 'is currently <span style="color: #55FF55;">no queue</span>.'}`
+        : `The <i>Alpha Hypixel Network</i> is currently <span style="color: #FF5555;">closed</span>.<br>The player limit is ${max}, which ${max == 0 ? '' : 'usually'} means the server is closed to the public.${online == 0 ? '' : `<br>There ${online == 1 ? 'is 1 player' : `are ${online} players`} online, but they are likely just ${online == 1 ? 'an admin' : `admins${online > 10 ? ' and testers' : ''}`}.`}`
+        ;
+}
+
+function checkConnection() {
+    if (window.navigator.onLine != connected) {
+        console.log(`[${new Date().toLocaleString()}] Network connection ${connected ? 'lost' : 'restored'}`);
+        document.getElementById('internet-status-message').innerHTML = connected ? '<span style="color: #FF8888;">🗙 No Network Connection</span>' : '';
+    }
+    return connected = window.navigator.onLine;
 }
 
 async function updateMessage() {
+    if (!checkConnection()) return;
     var alphaStatus = await fetchAlphaData();
-    console.log(alphaStatus);
+    console.log(`[${new Date(alphaStatus.timestampMilliseconds).toLocaleString()}] ${alphaStatus.online}/${alphaStatus.max}`);
+    // CSV style: `${new Date(alphaStatus.timestampMilliseconds).toISOString()},${alphaStatus.online},${alphaStatus.max}`
     document.getElementById('status-with-slash').innerText = alphaStatus.online + '/' + alphaStatus.max;
-    document.getElementById('alpha-status-message').innerHTML = generateMessage(alphaStatus.max, alphaStatus.online);
+    document.getElementById('alpha-status-message').innerHTML = generateMessage(alphaStatus.max, alphaStatus.online, alphaStatus.timestampMilliseconds);
 }
 
 function stop() {
     clearInterval(intervalID);
 }
 
-function restart() {
+function restart(status) {
     stop();
+    if (status !== undefined) prevData.alphaOpen = status;
     updateMessage();
-    intervalID = setInterval(updateMessage, 60000);
+    intervalID = setInterval(updateMessage, intervalDelay);
 }
 
 function openPopup() {
@@ -75,4 +68,5 @@ if (window.location.search.match(/[?&]popup/)) {
     document.getElementById('popup-button-container').style.display = 'none';
 }
 
+if (!checkConnection()) document.getElementById('alpha-status-message').innerHTML = '';
 restart();
